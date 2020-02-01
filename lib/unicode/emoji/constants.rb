d901f5d39dd5ec97616a04f0758d7298c9de31e0
Jan Lelis <hi@ruby.consulting>
2020-02-01 21:39:59 +0100

    Release v2.4.0

--- lib/unicode/emoji/constants.rb
+++ lib/unicode/emoji/constants.rb
@@ -2,13 +2,14 @@
 
 module Unicode
   module Emoji
-    VERSION = "2.3.1"
-    EMOJI_VERSION = "12.1"
+    VERSION = "2.4.0"
+    EMOJI_VERSION = "13.0"
     CLDR_VERSION = "36"
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + "/../../../data/").freeze
     INDEX_FILENAME = (DATA_DIRECTORY + "/emoji.marshal.gz").freeze
 
-    ENABLE_NATIVE_EMOJI_UNICODE_PROPERTIES = false # As of Ruby 2.6.1, Emoji version 11 is included
+    ENABLE_NATIVE_EMOJI_UNICODE_PROPERTIES = false
+    # As of Ruby 2.7.0, Emoji version 12.1 is included
   end
 end
 
