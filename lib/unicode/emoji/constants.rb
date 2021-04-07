e878151683468d4d79dcad33d0a42089d60e640f
Jan Lelis <hi@ruby.consulting>
2021-04-07 21:36:12 +0200

    Update CLDR to 39

--- lib/unicode/emoji/constants.rb
+++ lib/unicode/emoji/constants.rb
@@ -4,7 +4,7 @@ module Unicode
   module Emoji
     VERSION = "2.7.1"
     EMOJI_VERSION = "13.1"
-    CLDR_VERSION = "38.1"
+    CLDR_VERSION = "39"
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + "/../../../data/").freeze
     INDEX_FILENAME = (DATA_DIRECTORY + "/emoji.marshal.gz").freeze
   end
