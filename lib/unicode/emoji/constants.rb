505c78b682dc81b2511b7dde2bf18bbb9d60b515
Radosław Bułat <radek.bulat@gmail.com>
2021-09-30 22:04:07 +0200

    Make all regexes constants lazy loaded from pregenerated files

--- lib/unicode/emoji/constants.rb
+++ lib/unicode/emoji/constants.rb
@@ -5,8 +5,25 @@ module Unicode
     VERSION = "2.9.0"
     EMOJI_VERSION = "14.0"
     CLDR_VERSION = "39"
-    DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + "/../../../data/").freeze
+    DATA_DIRECTORY = File.expand_path('../../../data', __dir__).freeze
     INDEX_FILENAME = (DATA_DIRECTORY + "/emoji.marshal.gz").freeze
+
+    PROPERTY_NAMES = {
+      E: "Emoji",
+      B: "Emoji_Modifier_Base",
+      M: "Emoji_Modifier",
+      C: "Emoji_Component",
+      P: "Emoji_Presentation",
+      X: "Extended_Pictographic",
+    }.freeze
+
+    EMOJI_VARIATION_SELECTOR      = 0xFE0F
+    TEXT_VARIATION_SELECTOR       = 0xFE0E
+    EMOJI_TAG_BASE_FLAG           = 0x1F3F4
+    CANCEL_TAG                    = 0xE007F
+    TAGS                          = [*0xE0020..0xE007E].freeze
+    EMOJI_KEYCAP_SUFFIX           = 0x20E3
+    ZWJ                           = 0x200D
+    REGIONAL_INDICATORS           = [*0x1F1E6..0x1F1FF].freeze
   end
 end
-
