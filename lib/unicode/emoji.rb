fbb25d712fd884bdf531b4f313d55b1126434d8d
Radosław Bułat <radek.bulat@gmail.com>
2021-10-01 18:40:25 +0200

    Add support for generating regexes with native properties

--- lib/unicode/emoji.rb
+++ lib/unicode/emoji.rb
@@ -8,19 +8,24 @@ module Unicode
   module Emoji
     autoload :INDEX, File.expand_path('emoji/index', __dir__)
 
-    %i[
+    %w[
       EMOJI_CHAR EMOJI_CHAR EMOJI_PRESENTATION TEXT_PRESENTATION EMOJI_COMPONENT EMOJI_MODIFIER_BASES
-      EMOJI_MODIFIERS EXTENDED_PICTOGRAPHIC EXTENDED_PICTOGRAPHIC_NO_EMOJ EMOJI_KEYCAPS VALID_REGION_FLAGS
+      EMOJI_MODIFIERS EXTENDED_PICTOGRAPHIC EXTENDED_PICTOGRAPHIC_NO_EMOJI EMOJI_KEYCAPS VALID_REGION_FLAGS
       VALID_SUBDIVISIONS RECOMMENDED_SUBDIVISION_FLAGS RECOMMENDED_ZWJ_SEQUENCES LIST LIST_REMOVED_KEYS
     ].each do |const_name|
       autoload const_name, File.expand_path('emoji/lazy_constants', __dir__)
     end
 
-    %i[
+    generated_constants_dirpath = File.expand_path(
+      EMOJI_VERSION == Unicode::Version.emoji_version ? "emoji/generated_native/" : "emoji/generated/",
+      __dir__
+    )
+
+    %w[
       REGEX REGEX_VALID REGEX_WELL_FORMED REGEX_BASIC REGEX_TEXT REGEX_ANY REGEX_INCLUDE_TEXT
       REGEX_VALID_INCLUDE_TEXT REGEX_WELL_FORMED_INCLUDE_TEXT REGEX_PICTO REGEX_PICTO_NO_EMOJI
     ].each do |const_name|
-      autoload const_name, File.expand_path("emoji/generated/#{const_name.downcase}", __dir__)
+      autoload const_name, File.join(generated_constants_dirpath, const_name.downcase)
     end
 
     def self.properties(char)
